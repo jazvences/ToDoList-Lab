fed849f80ddfb5d15975a463e36fec06a5fd05b7
Jazmin Vences <45127211+jazvences@users.noreply.github.com>
2020-12-22 14:07:16 -0600

    Initial commit

---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,22 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+
+import UIKit
+
+protocol ToDoCellDelegate {
+    func checkMarkTapped(_ sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var delegate: ToDoCellDelegate?
+    
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        delegate?.checkMarkTapped(self)
+    }
+}
